dd890e6f5e752b5cb4523c6b18f91dcbaa08dc73
zynpakclgl <55785717+zynpakclgl@users.noreply.github.com>
2022-06-20 16:27:34 +0300

    login commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="main">
+    <div className='App'>
+      <input type={"email"} placeholder="Email" />
+      <input type={"password"} placeholder="Password"/>
+
+      <button>Create Account</button>
+      <button>Sign in</button>
+
+    </div>
+  </div>
+  );
+}
+
+export default App;
